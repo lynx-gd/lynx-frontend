3fffa37f03c37058eb04c8e10e0f95e6e46cda55
ohadgreen <ohad.green@gmail.com>
2019-03-15 08:13:24 +0200

    asset details page

--- src/components/pages/asset-detail/Main.jsx
+++ src/components/pages/asset-detail/Main.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import { Box, Button, Text } from "grommet";
+import { Box, Button, Text, Image, Heading } from "grommet";
+import { FaBed, FaMapMarked, FaEthereum } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import AssetGrid from "../../common/AssetGrid";
 
-const LeftSide = () => (
-  <Box align="center" justify="center">
-    <Text weight="bold">Purchase an asset</Text>
-  </Box>
-);
+const PLACEHOLDER =
+  "https://c1.staticflickr.com/3/2575/3710599253_3710a2d000_b.jpg";
 
 const ActionButton = styled(Button)`
+  margin-top: 20px;
   height: 40px;
   color: ${props => props.theme.global.colors.brand};
-  width: 220px;
+  width: 180px;
   font-size: "0.9rem";
   border-radius: 20px;
   text-align: center;
-
   font-weight: bold;
   border: 1px solid;
   border-color: ${props => props.theme.global.colors.brand};
@@ -27,31 +24,49 @@ const ActionButton = styled(Button)`
   }
 `;
 
-const PrimaryButton = styled(ActionButton)`
-  background-image: linear-gradient(81deg, #ec2383, #e63296 25%, #df42ab);;
-  color: white;
+const Ethereum = styled(FaEthereum)`
+  fill: ${props => props.theme.global.colors.focus};
+  opacity: 0.4;
 `;
 
-const RightSide = () => (
-  <Box direction="row" gap="medium">
-    <Link to="/add-asset">
-      <PrimaryButton label="New Asset" plain />
-    </Link>
-    <ActionButton label="Collect Dividend" plain />
-  </Box>
-);
+const AssetDesc = styled(Text)`
+  width: 185px;
+  height: 22px;
+  font-family: Montserrat;
+  font-size: 18px;
+  font-weight: 600;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.22;
+  letter-spacing: 0.4px;
+  text-align: left;
+  color: #08132c;
+`;
 
-const ActionLine = () => (
-  <Box direction="row" pad={{horizontal:"small"}} justify="between">
-    <LeftSide />
-    <RightSide />
-  </Box>
-);
-
-const Main = ({ assets }) => (
-  <Box pad={{ horizontal: "large", vertical: "large" }}>
-    <h1>test</h1>
-  </Box>
-);
+const Main = (props) => {
+  const { assets, id } = props;
+  const myasset = assets.find(asset => asset.id == id);
+  console.log(myasset);
+  return (<Box direction="row" pad="large">
+    <Image
+        gap="small"
+        height="380px"
+        width="600px"
+        src={
+          myasset.images && myasset.images.length > 0
+            ? `http://0.0.0.0:5000/images/${myasset.images[0]}`
+            : PLACEHOLDER
+        }
+      />
+      <Box pad="large" direction="column">
+      <AssetDesc>{myasset.description}</AssetDesc>
+      <Text style={{paddingTop:"10px"}}>{myasset.physicalAddress}</Text>
+      <Box direction="row" style={{paddingTop:"10px"}}><Ethereum /><Text>{myasset.value}</Text></Box>
+      <Text style={{paddingTop:"10px", fontSize:"14px"}}>Go Bangkok studio is located in the heart of Bangkok and is within walking distance to BTS &amp; Chao Praya River.
+This bright modern studio features a separate kitchen, a partitioned bedroom and a living area as well as a western style 3 piece bathroom</Text>
+<ActionButton label="Buy me!" plain />
+      </Box>
+  </Box>)
+};
 
 export default Main;
